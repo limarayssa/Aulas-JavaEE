e675e1201fa7469d5943366178f798a5a03ebe60
aluno <aluno@DSN1251230724>
2022-05-24 16:46:25 -0300

    Primeito commit.

---
+++ TelaLoginSimples/src/main/java/br/com/senai/rayssalima/application/LoginBean.java
@@ -0,0 +1,52 @@
+package br.com.senai.rayssalima.application;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named("loginb") 
+@RequestScoped
+public class LoginBean implements Serializable{
+
+	private String nome;
+	private String senha;
+	
+	@PostConstruct
+	public void onCreate() {
+		System.out.println("Bean criado");
+	}
+	
+	@PreDestroy
+	public void onDestroy () {
+		System.out.println("Bean será destruído");
+	}
+
+	public String doLogin() {
+		if ("abc".equals(nome) && "123".equals(senha)) {
+			return "sucesso";
+		}
+		
+		return null;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
