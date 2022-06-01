9a0a054c1f7d6028751ba6a1a08a2efdeeb32db3
Rayssa Lima <102378309+limarayssa@users.noreply.github.com>
2022-06-01 10:10:18 -0300

    Commit com comentários.

--- PrimeiroProjeto/src/main/java/br/com/senai/rayssalima/application/config/Config.java
+++ PrimeiroProjeto/src/main/java/br/com/senai/rayssalima/application/config/Config.java
@@ -3,8 +3,9 @@ package br.com.senai.rayssalima.application.config;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
-
+//força o eclipse a usar essa versão do jsf
 @FacesConfig(version = Version.JSF_2_3) 
+//dura enquanto o programa estiver sendo executado
 @ApplicationScoped
 public class Config {
 }
