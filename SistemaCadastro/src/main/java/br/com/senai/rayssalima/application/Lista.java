bb254c89e3d71dcd8c755948c238ab876f3846a9
Rayssa Lima <102378309+limarayssa@users.noreply.github.com>
2022-06-02 08:33:45 -0300

    Commit com comentários.

--- SistemaCadastro/src/main/java/br/com/senai/rayssalima/application/Lista.java
+++ SistemaCadastro/src/main/java/br/com/senai/rayssalima/application/Lista.java
@@ -9,44 +9,44 @@ public class Lista {
 
 	public static final List<Estado> ESTADOS;
 	public static final List<Interesse> INTERESSES;
-	
+	//inst√¢ncia das constantes
 	static {
 		ESTADOS = new ArrayList<>();
 		ESTADOS.add(new Estado("AC", "Acre"));
 		ESTADOS.add(new Estado("AL", "Alagoas"));
-		ESTADOS.add(new Estado("AP", "Amap·"));
+		ESTADOS.add(new Estado("AP", "Amap√°"));
 		ESTADOS.add(new Estado("AM", "Amazonas"));
 		ESTADOS.add(new Estado("BA", "Bahia"));
-		ESTADOS.add(new Estado("CE", "Cear·"));
+		ESTADOS.add(new Estado("CE", "Cear√°"));
 		ESTADOS.add(new Estado("DF", "Distrito Federal"));
-		ESTADOS.add(new Estado("ES", "EspÌrito Santo"));
-		ESTADOS.add(new Estado("GO", "Goi‚nia"));
-		ESTADOS.add(new Estado("MA", "Maranh„o"));
+		ESTADOS.add(new Estado("ES", "Esp√≠rito Santo"));
+		ESTADOS.add(new Estado("GO", "Goi√¢nia"));
+		ESTADOS.add(new Estado("MA", "Maranh√£o"));
 		ESTADOS.add(new Estado("MT", "Mato Grosso"));
 		ESTADOS.add(new Estado("MS", "Mato Grosso do Sul"));
 		ESTADOS.add(new Estado("MG", "Minas Gerais"));
-		ESTADOS.add(new Estado("PA", "Par·"));
-		ESTADOS.add(new Estado("PB", "ParaÌba"));
-		ESTADOS.add(new Estado("PR", "Paran·"));
+		ESTADOS.add(new Estado("PA", "Par√°"));
+		ESTADOS.add(new Estado("PB", "Para√≠ba"));
+		ESTADOS.add(new Estado("PR", "Paran√°"));
 		ESTADOS.add(new Estado("PE", "Pernambuco"));
-		ESTADOS.add(new Estado("PI", "PiauÌ"));
+		ESTADOS.add(new Estado("PI", "Piau√≠"));
 		ESTADOS.add(new Estado("RJ", "Rio de Janeiro"));
 		ESTADOS.add(new Estado("RN", "Rio Grande do Norte"));
 		ESTADOS.add(new Estado("RS", "Rio Grande do Sul"));
-		ESTADOS.add(new Estado("RO", "RondÙnia"));
+		ESTADOS.add(new Estado("RO", "Rond√¥nia"));
 		ESTADOS.add(new Estado("RR", "Roraima"));
 		ESTADOS.add(new Estado("SC", "Santa Catarina"));
-		ESTADOS.add(new Estado("SP", "S„o Paulo"));
+		ESTADOS.add(new Estado("SP", "S√£o Paulo"));
 		ESTADOS.add(new Estado("SE", "Sergipe"));
 		ESTADOS.add(new Estado("TO", "Tocantins"));
 		
 		INTERESSES = new ArrayList<>();	
 		INTERESSES.add(new Interesse(1, "Esportes"));
-		INTERESSES.add(new Interesse(2, "M˙sica"));
+		INTERESSES.add(new Interesse(2, "M√∫sica"));
 		INTERESSES.add(new Interesse(3, "Artes Marciais"));
 		INTERESSES.add(new Interesse(4, "Viagens"));
 		INTERESSES.add(new Interesse(5, "Cinema"));
-		INTERESSES.add(new Interesse(6, "DanÁa"));
+		INTERESSES.add(new Interesse(6, "Dan√ßa"));
 	}
 		
 }
