df1e6e6d4f239a8be3cca605b5eab080b4560aba
Rayssa Lima <102378309+limarayssa@users.noreply.github.com>
2022-06-02 09:07:49 -0300

    Commit com comentários.

--- Decorator/src/main/java/br/com/senai/rayssalima/application/config/Config.java
+++ Decorator/src/main/java/br/com/senai/rayssalima/application/config/Config.java
@@ -6,5 +6,6 @@ import javax.faces.annotation.FacesConfig.Version;
 //força o eclipse a usar essa versão do jsf
 @FacesConfig(version = Version.JSF_2_3) 
 @ApplicationScoped
+//essa configuração dura enquanto a aplicação estiver sendo usada
 public class Config {
 }
