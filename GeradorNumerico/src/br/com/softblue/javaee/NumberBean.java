34b4b7b6e94545b4d06420840bdeec71dfc25b19
Rayssa Lima <102378309+limarayssa@users.noreply.github.com>
2022-06-09 15:31:18 -0300

    Add files via upload

---
+++ GeradorNumerico/src/br/com/softblue/javaee/NumberBean.java
@@ -0,0 +1,67 @@
+package br.com.softblue.javaee;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@RequestScoped
+public class NumberBean implements Serializable {
+	
+	//instância dos método dos números aleatórios
+	private static Random random = new Random();
+
+	private Integer qtde;
+	private Integer min;
+	private Integer max;
+	
+	private List<Integer> numeros = new ArrayList<>();
+
+	//gera os números aleatórios conforme a quantidade máxima de números escolhidos
+	// conforme os vaores mínimos e os valores máximos
+	public String gerar() {
+		for (int i = 0; i < qtde; i++) {
+			int n = random.nextInt(max - min) + min + 1;
+			numeros.add(n);
+		}
+		
+		return null;
+	}
+
+	public Integer getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(Integer qtde) {
+		this.qtde = qtde;
+	}
+
+	public Integer getMin() {
+		return min;
+	}
+
+	public void setMin(Integer min) {
+		this.min = min;
+	}
+
+	public Integer getMax() {
+		return max;
+	}
+
+	public void setMax(Integer max) {
+		this.max = max;
+	}
+
+	public List<Integer> getNumeros() {
+		return numeros;
+	}
+
+	public void setNumeros(List<Integer> numeros) {
+		this.numeros = numeros;
+	}
+}
